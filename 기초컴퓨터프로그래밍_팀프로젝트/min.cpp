d1077db3a7c9f7be5425aff094031fdcdcc0e30e
신동준 <shindj4613@outlook.com>
2019-05-31 01:06:59 +0900

    기본함수헤더다른파일들에게도추가

--- 기초컴퓨터프로그래밍_팀프로젝트/min.cpp
+++ 기초컴퓨터프로그래밍_팀프로젝트/min.cpp
@@ -1,3 +1,4 @@
 #include "min.h"
+#include "basic_function.h"
 #include <CoalaMOD.h>
 #pragma comment(lib, "CoalaMOD.lib")
--- 기초컴퓨터프로그래밍_팀프로젝트/nyeng.cpp
+++ 기초컴퓨터프로그래밍_팀프로젝트/nyeng.cpp
@@ -1,3 +1,4 @@
 #include "nyeng.h"
+#include "basic_function.h"
 #include <CoalaMOD.h>
 #pragma comment(lib, "CoalaMOD.lib")
